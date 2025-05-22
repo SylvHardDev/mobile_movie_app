040d08d04f71e5ef57d5b4f98892e4aee71f382d
HardyNuklear <hardy.herinavalona@nuklear.io>
2025-05-22 15:41:13 +0300

    init project

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white dark:bg-black">
+      <Text className="text-dark-200 text-2xl font-bold">
+        app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
